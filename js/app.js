06a4eac301473cb30fe2a1763cfc69f957d88832
monsterstudios <info@monsterstudios.com.ng>
2020-05-17 10:05:30 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,50 @@
+$(function() {
+    'use strict'
+
+    $('[data-toggle="offcanvas"]').on('click', function() {
+        $('.offcanvas-collapse').toggleClass('open')
+    })
+
+    $('#AppGallery a').nivoLightbox({
+        effect: 'fall'
+    });
+
+    $("#main-slider").owlCarousel({
+        items: 1,
+        loop: true,
+        dots: true
+    });
+
+    $("#AppGallery").owlCarousel({
+        items: 4,
+        loop: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+
+    $("#sidebar").sticky({
+        topSpacing: 100,
+        bottomSpacing: 800
+    });
+
+    $(".collapse.show").each(function(){
+        $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+    });
+    
+    $(".collapse").on('show.bs.collapse', function(){
+        $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+    }).on('hide.bs.collapse', function(){
+        $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+    });
+
+});
