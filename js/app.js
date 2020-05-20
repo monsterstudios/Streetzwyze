b92a67ca011c8917231336db3265194705931eb9
monsterstudios <info@monsterstudios.com.ng>
2020-05-20 17:11:51 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -32,19 +32,102 @@ $(function() {
         }
     });
 
+    var sslider = $('#signup-slider');
+    $(sslider).owlCarousel({
+        items: 1,
+        loop: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    sslider.owlCarousel();
+    
+    $('.signup-merchant').click(function() {
+        sslider.trigger('prev.owl.carousel', [300]);
+    });
+
+    $('.signup-seller').click(function() {
+        sslider.trigger('next.owl.carousel', [300]);
+    });
+
+
     $("#sidebar").sticky({
         topSpacing: 100,
         bottomSpacing: 800
     });
 
-    $(".collapse.show").each(function(){
-        $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
-    });
-    
-    $(".collapse").on('show.bs.collapse', function(){
-        $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
-    }).on('hide.bs.collapse', function(){
-        $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+    var current_fs, next_fs, previous_fs; //fieldsets
+    var opacity;
+
+    $(".btn-next").click(function() {
+
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+
+        //Add Class Active
+        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+        //show the next fieldset
+        next_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate({ opacity: 0 }, {
+            step: function(now) {
+                // for making fielset appear animation
+                opacity = 1 - now;
+
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+                next_fs.css({ 'opacity': opacity });
+            },
+            duration: 600
+        });
     });
 
+    $(".btn-previous").click(function() {
+
+        current_fs = $(this).parent('fieldset');
+        previous_fs = $(this).parent('fieldset').prev();
+
+        //Remove class active
+        $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+        //show the previous fieldset
+        previous_fs.show();
+
+        //hide the current fieldset with style
+        current_fs.animate({ opacity: 0 }, {
+            step: function(now) {
+                // for making fielset appear animation
+                opacity = 1 - now;
+
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+                previous_fs.css({ 'opacity': opacity });
+            },
+            duration: 600
+        });
+    });
+
+    $('.radio-group .radio').click(function() {
+        $(this).parent().find('.radio').removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+    $(".reservation-submit").click(function() {
+        return false;
+    })
 });
